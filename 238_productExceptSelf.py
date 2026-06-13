6f32d007780ec5ce559f769c8bf3a6a5ee82cc1a
abhishek-kumar <abhishekkumar@abhisheks-Mac-mini.local>
2026-06-13 15:15:31 +0530

    solve 33/200

--- 238_productExceptSelf.py
+++ 238_productExceptSelf.py
@@ -2,13 +2,15 @@ nums = [1,2,3,4]
 Output =  [24,12,8,6]
 
 
-
-
 def productExceptSelf(nums) :
-    answer = []
-    for i in range(0,len(nums)):  
-        
+    result = []
 
+    for i in range(0,len(nums)):
+        for k in nums:   
+            nums.pop(i)
+
+    for j in nums:
+        
     
 
 
---
+++ 917_ReverseLetters.py
@@ -0,0 +1,39 @@
+'''
+
+917. Reverse Only Letters
+
+Given a string s, reverse the string according to the following rules:
+
+All the characters that are not English letters remain in the same position.
+All the English letters (lowercase or uppercase) should be reversed.
+Return s after reversing it.
+
+ 
+
+Example 1:
+
+Input: s = "ab-cd"
+Output: "dc-ba"
+Example 2:
+
+Input: s = "a-bC-dEf-ghIj"
+Output: "j-Ih-gfE-dCba"
+Example 3:
+
+Input: s = "Test1ng-Leet=code-Q!"
+Output: "Qedo1ct-eeLg=ntse-T!"
+
+'''
+
+
+s = "ab-cd"
+Output =  "dc-ba"
+
+
+def reverseOnlyLetters(s):
+    ltr = ''
+    x = s.reverse()
+    print(x)
+
+
+reverseOnlyLetters(s)
